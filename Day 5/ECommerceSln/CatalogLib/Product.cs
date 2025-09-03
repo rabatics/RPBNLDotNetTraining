c166c26441864fa1bdef42f4dedec5a127f123e4
Rajesh Basrur <rajeshb2@nedbank.co.za>
2025-09-03 21:22:14 +0200

    Day5 changes ecommerce sln

---
+++ Day 5/ECommerceSln/CatalogLib/Product.cs
@@ -0,0 +1,18 @@
+﻿﻿namespace CatalogLib
+{
+
+
+    //POCO - Plain Old CLR Object
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} (ID: {Id}) - ${Price}: {Description}";
+        }
+    }
+}
